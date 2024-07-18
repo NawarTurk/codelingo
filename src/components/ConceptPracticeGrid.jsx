0dc541d5ea419e63bc287109d1f55d40028b761f
nawarturk <nawarturk11@gmail.com>
2024-07-18 16:33:02 -0400

    feat: initial implementation of tracking incorrect answer categories for user revision

--- src/components/ConceptPracticeGrid.jsx
+++ src/components/ConceptPracticeGrid.jsx
@@ -17,13 +17,14 @@ export default function ConceptPracticeGrid({ questions }) {
 
   function getResults() {
     const result = answersState.reduce(
-      (acc, answer) => {
+      (acc, answer, index) => {
         if (answer) {
           acc.correct++;
           acc.remainingQuestions--;
         } else if (answer === false) {
           acc.wrong++;
           acc.remainingQuestions--;
+          acc.conceptsToRevise.push(questions[index].category);
         }
         return acc;
       },
@@ -31,13 +32,18 @@ export default function ConceptPracticeGrid({ questions }) {
         correct: 0,
         wrong: 0,
         remainingQuestions: questions.length,
+        conceptsToRevise: [],
       }
     );
 
     const totalAnswers = result.wrong + result.correct;
 
     if (totalAnswers === 0) {
-      return { score: "0%", remainingQuestions: result.remainingQuestions };
+      return {
+        score: "0%",
+        remainingQuestions: result.remainingQuestions,
+        conceptsToRevise: result.conceptsToRevise,
+      };
     }
 
     const resultPercentage =
@@ -46,19 +52,28 @@ export default function ConceptPracticeGrid({ questions }) {
     return {
       score: resultPercentage,
       remainingQuestions: result.remainingQuestions,
+      conceptsToRevise: result.conceptsToRevise,
     };
   }
 
-  const { score, remainingQuestions } = getResults();
+  const { score, remainingQuestions, conceptsToRevise } = getResults();
 
   return (
     <div>
       <button onClick={() => handleReset()}>Reset</button>
-      <p>{score}</p>
+      <p>Score: {score}</p>
       <p>
         remaining questions:{" "}
         {remainingQuestions === 0 ? "All Done! Good Job" : remainingQuestions}
       </p>
+      <div>
+        <h3>Concepts you got wrong:</h3>
+        <div>
+          {conceptsToRevise.map((concept) => (
+            <span> {concept},</span>
+          ))}
+        </div>
+      </div>
       {questions.map((question, questionIndex) => (
         <div key={questionIndex}>
           <h1>{question.questionText}</h1>
